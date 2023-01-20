70e14cfcb00ca09879ac937996f4ff022a67f098
petersahanaya <petersahanaya09@gmail.com>
2023-01-20 23:35:07 +0900

    Update Image

--- src/components/home/Header.svelte
+++ src/components/home/Header.svelte
@@ -4,7 +4,7 @@
 
 <header class="w-screen bg-stone-900 h-[12vh] flex justify-around items-center fixed top-0 z-20 right-0">
     <div class="flex justify-around items-center gap-2">
-        <img class="w-[50px] h-[50px] md:w-[80px] md:h-[80px] shadow-md shadow-white/10 rounded-full object-cover" width="40" height="40" src="/profile.jpg" alt="peter sahanaya photo profile" />
+        <img class="w-[50px] h-[50px] md:w-[80px] md:h-[80px] shadow-md shadow-white/10 rounded-full object-cover" width="40" height="40" src="/profile.webp" alt="peter sahanaya photo profile" />
         <p class="text-[.8rem] md:text-xl text-stone-200">Peter Sahanaya</p>
     </div>
     <div on:click={() => isSideOpen.set(!$isSideOpen)} class={`w-[2rem] md:w-[3rem] lg:hidden flex flex-col justify-center gap-2 relative z-20 transition-[200ms] ${!$isSideOpen && "hover:gap-3 md:hover:gap-5"}`}>
